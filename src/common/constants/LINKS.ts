3f243251c92eeb5d2ad86a2efdb19dede4aaebab
sooy <ssy67366736@gmail.com>
2024-09-09 17:07:04 +0900

    feat: links

--- src/common/constants/LINKS.ts
+++ src/common/constants/LINKS.ts
@@ -1,8 +1,8 @@
 // 외부 링크
 export const LINKS = {
-  twitter: 'https://x.com/blockwavelabs',
-  telegrem: '',
+  twitter: 'https://x.com/MytetherBwl',
+  telegrem: 'https://t.me/mytetherQVE',
   medium: '',
-  github: '',
+  github: 'https://github.com/ziptalk/mytether-frontend-typescript',
   docs: '',
 };
