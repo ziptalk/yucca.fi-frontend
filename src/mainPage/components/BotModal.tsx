6cf40f51e3554ad89b629185253c82b1b362361a
sooy <ssy67366736@gmail.com>
2024-09-09 15:03:49 +0900

    fix: minval

--- src/mainPage/components/BotModal.tsx
+++ src/mainPage/components/BotModal.tsx
@@ -23,7 +23,7 @@ import { USDTTokenAddress } from '../../common/contracts/tokenAddress';
 import { convertTokenBalance } from '../../common/utils/convertTokenBalance';
 
 const base_url = import.meta.env.VITE_BASE_URL;
-const MINVAL = 0;
+const MINVAL = 10;
 const BotModal = ({
   isOpen,
   onClose,
