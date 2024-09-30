be1c82d3c6200f36d2bdfe22a2d77279acffd97c
sooy <ssy67366736@gmail.com>
2024-09-30 17:19:16 +0900

    feat: link

--- src/common/constants/LINKS.ts
+++ src/common/constants/LINKS.ts
@@ -1,7 +1,7 @@
 // 외부 링크
 export const LINKS = {
-  twitter: '',
-  telegrem: '',
+  twitter: 'https://x.com/yukapainae12306',
+  telegrem: 'https://t.me/yuccafi',
   medium: '',
   github: '',
   docs: 'https://blockwavelabs-1.gitbook.io/yucca.fi',
