c11709b1025b4f06b743be17e8d4aba30e56cf3b
sooy <ssy67366736@gmail.com>
2024-12-02 13:07:03 +0900

    feat: onboarding3 scroll

--- src/onboarding/assets/Onboarding3/0_index.ts
+++ src/onboarding/assets/Onboarding3/0_index.ts
@@ -3,9 +3,24 @@ import onboarding3Left from './onboarding3_left.webp';
 import onboarding3Right from './onboarding3_right.webp';
 import onboarding3_mobile from './section3_mobile.webp';
 
+import IcCurvedArrowDownRight from './curvedArrowDownRight.svg?react';
+import IcCurvedArrowDownLeft from './curvedArrowDownLeft.svg?react';
+import IcCurvedArrowUpRight from './curvedArrowUpRight.svg?react';
+import IcCurvedArrowUpLeft from './curvedArrowUpLeft.svg?react';
+import IcWaveLine from './waveLine.svg?react';
+import IcWaveArrow from './waveArrow.svg?react';
+import IcHome from './icHome.svg?react';
+
 export {
   onboarding3Left,
   onboarding3Middle,
   onboarding3Right,
   onboarding3_mobile,
+  IcCurvedArrowDownLeft,
+  IcCurvedArrowDownRight,
+  IcCurvedArrowUpLeft,
+  IcCurvedArrowUpRight,
+  IcWaveArrow,
+  IcWaveLine,
+  IcHome,
 };
