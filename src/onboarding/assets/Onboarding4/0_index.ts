e43da03eb2df5034819f37e26209f28118c96f15
sooy <ssy67366736@gmail.com>
2024-12-05 14:02:39 +0900

    refac: onboarding img path

--- src/onboarding/assets/Onboarding4/0_index.ts
+++ src/onboarding/assets/Onboarding4/0_index.ts
@@ -1,3 +1,14 @@
 import IcRightArrow from './rightArrow.svg?react';
 
-export { IcRightArrow };
+import imgDepositFunds from './section4_depositFunds.webp';
+import imgGenerateProfits from './section4_generateProfits.webp';
+import imgLiquifyPosition from './section4_liquifyPosition.webp';
+import imgWithdrawAssets from './section4_withdrawAssets.webp';
+
+export {
+  IcRightArrow,
+  imgDepositFunds,
+  imgGenerateProfits,
+  imgLiquifyPosition,
+  imgWithdrawAssets,
+};
